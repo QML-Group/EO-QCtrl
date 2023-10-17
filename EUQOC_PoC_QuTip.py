8edfd416770352419899ca6d38ba82e395182852
SebFqt <sebastiaan.fauquenot@gmail.com>
2023-10-17 11:13:07 +0200

    Implemented energetic cost function in QuTip

--- EUQOC_PoC_QuTip.py
+++ EUQOC_PoC_QuTip.py
@@ -12,7 +12,7 @@ from qutip.ui.progressbar import TextProgressBar
 # Define total time 
 
 T = 2 * np.pi # Total evolution time
-times = np.linspace(0, T, 10) # Total time array in 500 timesteps
+times = np.linspace(0, T, 500) # Total time array in 500 timesteps
 
 # Define target unitary and Hamiltonian
 
@@ -62,3 +62,21 @@ plt.show()
 
 print(_overlap(U_target, result.U_f).real, abs(_overlap(U_target, result.U_f)) ** 2)
 
+# Calculate and Plot Energetic Cost
+
+norm = np.linalg.norm(H0) + result.u[0,0]*np.linalg.norm(H_ops[0]) + result.u[0,1]*np.linalg.norm(H_ops[1]) + result.u[0,2]*np.linalg.norm(H_ops[2]) + result.u[0,3]*np.linalg.norm(H_ops[3]) + result.u[0,4]*np.linalg.norm(H_ops[4]) + result.u[0,5]*np.linalg.norm(H_ops[5]) + result.u[0,6]*np.linalg.norm(H_ops[6])
+
+energetic_cost = 0
+list = []
+timestep = max(times)/len(times)
+
+for i in range(len(times)):
+    energetic_cost += norm[i]
+    list.append(energetic_cost*timestep)
+
+
+plt.plot(times, list)
+plt.xlabel('Time t')
+plt.ylabel('Energetic Cost (a.u.)')
+plt.title('Energetic Cost over Time')
+plt.show()
