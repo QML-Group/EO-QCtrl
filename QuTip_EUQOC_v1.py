c5c7eeb487ebdcb1ec183fa20ea37f388691cd7b
SebFqt <sebastiaan.fauquenot@gmail.com>
2023-10-18 10:47:04 +0200

    Clean up & make new "v1" file

---
+++ QuTip_EUQOC_v1.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import time
+import numpy as np
+from qutip import *
+from qutip.control import *
+from qutip.qip.operations import cnot
+from qutip.qip.operations import cphase
+from qutip.control.grape import plot_grape_control_fields, _overlap, grape_unitary_adaptive, cy_grape_unitary
+from scipy.interpolate import interp1d
+from qutip.ui.progressbar import TextProgressBar
+
+
