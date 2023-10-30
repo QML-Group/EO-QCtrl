ef2ea33fe9e1211ab046788f6d8bbacb85a5ca25
SebFqt <sebastiaan.fauquenot@gmail.com>
2023-10-30 10:30:54 +0100

    Scratch model functions

---
+++ Scratch_Model/functions.py
@@ -0,0 +1,100 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import unitary_group
+
+"""
+
+This File Contains Functions that are used in the "input.py" and "main.py" files to calculate and simulate
+Quantum Optimal Control problems in python
+
+"""
+
+def tensor(a, b):
+    """
+    Returns tensor product between two matrices
+    """
+
+    return np.kron(a, b)
+
+def identity(N):
+    """
+    Returns Identity Matrix with Dimension 'N'
+    """
+
+    return np.identity(N)
+
+def sigmax():
+    """
+    Returns Pauli-x Matrix 
+    """
+
+    return np.array([[0,1],
+                     [1,0]])
+
+def sigmay():
+    """
+    Returns Pauli-y Matrix 
+    """
+
+    return np.array([[0, -1j],
+                     [1j, 0]])
+
+def sigmaz():
+    """
+    Returns Pauli-z Matrix 
+    """
+
+    return np.array([[1, 0],
+                     [0, 1]])
+
+def cnot():
+    """
+    Returns CNOT Unitary Gate 
+    """
+
+    return np.array([[1, 0, 0, 0],
+                     [0, 1, 0, 0],
+                     [0, 0, 0, 1],
+                     [0, 0, 1, 0]])
+
+def rand_unitary(N):
+    """
+    Returns N-Dimenstional Random Unitary 
+    """
+
+    x = unitary_group.rvs(N)
+    y = np.dot(x, x.conj().T)
+
+    return y
+
+def GRAPE(U_Target, H_Static, H_Control, Iterations, Timesteps, U_Start = None):
+
+    """
+    This Function Implements the GRAPE (Gradient Ascent Pulse Engineering) Algorithm using NumPy in Python
+    Calculates control pulses for the Hamitonian operators in H_Control so that the unitary U_Target is realized.
+
+    Parameters 
+    ----------
+
+    U_Target : Target Unitary Evolution Operator
+
+    H_Static : Static/Drift Hamiltonian Term
+
+    H_Control : Control Hamiltonian containing operators that can be tuned in the Hamiltonian via the control fields
+
+    Iterations : Number of GRAPE iterations 
+
+    Timesteps : Number of timesteps 'N' for time discretization
+
+    U_Start : Optional array with initial control pulse values
+    
+
+    Returns 
+    ----------
+        
+    TBD
+
+    """
+
+    
+    pass 
---
+++ Scratch_Model/input.py
@@ -0,0 +1,67 @@
+import numpy as np
+from functions import *
+
+"""
+
+This File Contains Inputs that are used in the "main.py" file to calculate and simulate
+Quantum Optimal Control problems in python
+
+"""
+
+T = 2 * np.pi # Total gate time
+
+Iterations_GRAPE = 100 # Total number of GRAPE iterations
+
+Timesteps = 500 # Total number of timesteps to discretize the time space
+
+H_Static_1 = np.pi * (tensor(sigmaz(), identity(2)) + tensor(identity(2), sigmaz())) + (1/2) * np.pi * tensor(sigmaz(), sigmaz()) # Static Drift Hamiltonian 1 Inlcuding Interaction Term
+
+U_Target_CNOT = cnot()
+
+U_Target_Rand = rand_unitary(4)
+
+H_Control_1 =  [tensor(sigmax(), identity(2)), # General 2 qubit Hamiltonian with X, Y, and Z interaction terms 
+         tensor(sigmay(), identity(2)),
+         tensor(sigmaz(), identity(2)),
+         tensor(identity(2), sigmax()),
+         tensor(identity(2), sigmay()),
+         tensor(identity(2), sigmaz()),
+         tensor(sigmax(), sigmax()) +
+         tensor(sigmay(), sigmay()) +
+         tensor(sigmaz(), sigmaz())] 
+
+H_Labels_1 = [r'$u_{1x}$', r'$u_{1y}$', r'$u_{1z}$', # Labels for H_Control_1 (optional for plotting)
+            r'$u_{2x}$', r'$u_{2y}$', r'$u_{2z}$',
+            r'$u_{xx}$',
+            r'$u_{yy}$',
+            r'$u_{zz}$'
+            ] 
+
+H_Control_2 = [tensor(sigmax(), identity(2)), # Control Hamiltonian 2: no Y terms 
+               tensor(sigmaz(), identity(2)),
+               tensor(identity(2), sigmax()),
+               tensor(identity(2), sigmaz()),
+               tensor(sigmax(), sigmax()) +
+               tensor(sigmaz(), sigmaz())
+               ] 
+
+H_Labels_2 = [r'$u_{1x}$', r'$u_{1z}$', # Labels for H_Control_2 (optional for plotting)
+              r'$u_{2x}$', r'$u_{2z}$', 
+              r'$u_{xx}$', r'$u_{zz}$'] 
+
+H_Control_3 = [tensor(sigmax(), sigmax()), # Control Hamiltonian 3: No single qubit terms
+               tensor(sigmay(), sigmay()), 
+               tensor(sigmaz(), sigmaz())]
+
+H_Labels_3 = [r'$u_{xx}$', # Labels for H_Control_3 (optional for plotting)
+              r'$u_{yy}$', 
+              r'$u_{zz}$'] 
+
+H_Control_4 = [tensor(sigmax(), identity(2)), # Control Hamiltonian 2: no Y and Z terms
+               tensor(identity(2), sigmax()),
+               tensor(sigmax(), sigmax())
+               ] 
+
+H_Labels_4 = [r'$u_{1x}$', # Labels for H_Control_4 (optional for plotting)
+              r'$u_{2x}$', 
+              r'$u_{xx}$'] 
