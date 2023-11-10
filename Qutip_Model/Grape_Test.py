23065699b22971e6f004fe92a7873030557b5d9f
SebFqt <sebastiaan.fauquenot@gmail.com>
2023-11-10 15:12:57 +0100

    Changed gradient energetic cost

--- Qutip_Model/Grape_Test.py
+++ Qutip_Model/Grape_Test.py
@@ -32,7 +32,7 @@ class GRAPEResult:
         self.H_t = H_t
         self.U_f = U_f
 
-def cy_grape_inner(U, u, r, J, M, U_b_list, U_f_list, H_ops, dt, eps, weight_ec, weight_fidelity, alpha, beta, phase_sensitive, use_u_limits, u_min, u_max):
+def cy_grape_inner(U, u, r, J, M, U_b_list, U_f_list, H_ops, dt, eps, weight_ec, weight_fidelity, H_Control, alpha, beta, phase_sensitive, use_u_limits, u_min, u_max):
     
     """
     Perform one iteration of GRAPE control pulse
@@ -88,9 +88,9 @@ def cy_grape_inner(U, u, r, J, M, U_b_list, U_f_list, H_ops, dt, eps, weight_ec,
         for j in range(J): # Loop over all control fields 
             Q = 1j * dt * H_ops[j] @ U_f_list[m] # Forward propagator storing (i * dt * )
             
-            du = -2 * weight_fidelity * cy_overlap(P, Q) * cy_overlap(U_f_list[m], P) # Calculate Gradient 
+            du = -2 * weight_fidelity * cy_overlap(P, Q) * cy_overlap(U_f_list[m], P) # Calculate Gradient Fidelity
 
-            du += -2 * weight_ec * u[r, j, m] * dt
+            du += -2 * weight_ec * u[r, j, m] * dt * (H_Control[j].dag() * H_Control[j]).tr() # Calculate Gradient Energetic Cost
 
             u[r + 1, j, m] = u[r, j, m] + eps * du.real # Update control pulses according to gradient (gradient * distance to move along gradient)
 
@@ -141,7 +141,7 @@ def cy_grape_unitary(U, H0, H_ops, R, times, weight_ec, weight_fidelity, eps=Non
             U_b = U_list[M - 2 - n].T.conj().tocsr() * U_b # Backward propagator 
 
         cy_grape_inner(U.data, u, r, J, M, U_b_list, U_f_list, H_ops_data, # Calculate Gradient based on cy_grape_inner function --> update control parameters --> do R times
-                       dt, eps, weight_ec, weight_fidelity, alpha_val, beta_val, phase_sensitive,
+                       dt, eps, weight_ec, weight_fidelity, H_ops, alpha_val, beta_val, phase_sensitive,
                        use_u_limits, u_min, u_max)
 
     H_td_func = [H0] + [[H_ops[j], u[-1, j, :]] for j in range(J)] # Store total Hamiltonian over time 
--- Qutip_Model/main.py
+++ Qutip_Model/main.py
@@ -13,13 +13,47 @@ using built in functions from QuTip
 
 """
 
-EC, F = CalculateOptimalFieldEnergeticCost(U_target_CNOT, H_Static_2, # Run algorithm with set of initial parameters
-                                           H_Control_5, Iterations_GRAPE, 
-                                           Timesteps, H_Labels_5, 
-                                           weight_ec = 0.0, weight_fidelity= 1.0,
-                                           Plot_Control_Field = True, Plot_Tomography = False) 
+#EC, F = CalculateOptimalFieldEnergeticCost(U_target_CNOT, H_Static_2, # Run algorithm with set of initial parameters
+#                                           H_Control_5, Iterations_GRAPE, 
+#                                           Timesteps, H_Labels_5, 
+#                                           weight_ec = 0.0, weight_fidelity= 1.0,
+#                                           Plot_Control_Field = True, Plot_Tomography = False) 
 
-Output = f"""  
+Weights = np.arange(1, step = 0.1)
+EnergeticCost = []
+Fidelity = []
+
+for i in Weights:
+    EnergeticCost_i_j, Fidelity_i_j = CalculateOptimalFieldEnergeticCost(U_target_CNOT, H_Static_2,
+                                                                            H_Control_5, Iterations_GRAPE,
+                                                                            Timesteps, H_Labels_5,
+                                                                            weight_ec = i, weight_fidelity = 1-i,
+                                                                            Plot_Control_Field = False, Plot_Tomography = False)
+    EnergeticCost.append(EnergeticCost_i_j)
+    Fidelity.append(Fidelity_i_j)
+
+EnergeticCost_Normalized = EnergeticCost / max(EnergeticCost)
+Error_Rate = np.ones(len(Fidelity)) - Fidelity
+
+plt.plot(EnergeticCost_Normalized, Error_Rate, ls = '-', color = 'green', marker = 'd') 
+plt.xlabel('Energetic Cost')
+plt.ylabel('Infidelity')
+plt.title('Energetic Cost versus Infidelity')
+plt.grid()
+plt.show()
+
+#plt.plot(Weights, Error_Rate, label = 'Infidelity')
+#plt.plot(Weights, EnergeticCost_Normalized, label = 'Normalized Energetic Cost')
+#plt.xlabel('Weight Energetic Cost')
+#plt.ylabel('Infidelity & Normalized Energetic Cost')
+#plt.title('Infidelity and Normalized Energetic Cost as function of Energetic Cost Weight \n CNOT Gate, Timesteps = 500, GRAPE Iterations = 100')
+#plt.grid()
+#plt.legend()
+#plt.show()
+
+
+"""
+Output = f  
 
 **** PROGRAM OUTPUT & RESULTS ****
 
@@ -46,4 +80,4 @@ Output = f"""
     Number of Timesteps: {Timesteps}
 """
 
-print(Output) #Print Output
+#print(Output) #Print Output
