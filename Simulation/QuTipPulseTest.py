9448002cd7b0a68e68f7e20b19dd59f6263a72be
SebFqt <sebastiaan.fauquenot@gmail.com>
2023-12-27 15:46:58 +0100

    Qutip Pulse Processor simulation

--- Simulation/QuTipPulseTest.py
+++ Simulation/QuTipPulseTest.py
@@ -1,45 +1,62 @@
 import numpy as np 
 from qutip import basis, fidelity, identity, sigmax, sigmaz, tensor
 from qutip_qip.circuit import QubitCircuit
-from qutip_qip.device import OptPulseProcessor
 from qutip_qip.operations import expand_operator, toffoli
-import qutip_qip
 import matplotlib.pyplot as plt 
+from qutip.qip.device import Processor
+import functions as fc
 
-num_qubits = 2
-# Drift Hamiltonian
-H_d = 1 * np.pi * (tensor(sigmaz(), identity(2)) + tensor(identity(2), sigmaz())) + (1/2) * np.pi * tensor(sigmaz(), sigmaz())
-# The (single) control Hamiltonian
-H_c = [tensor(sigmax(), identity(2)),
-       tensor(identity(2), sigmax()),
-       tensor(sigmax(), sigmax())]
-              
-processor = OptPulseProcessor(num_qubits, drift=H_d)
 
-for operators in H_c:
-    processor.add_control(operators, targets = [0,1])
+N_q = 2
 
-qc = QubitCircuit(num_qubits)
-qc.add_gate("CNOT", targets = 1, controls = 0)
+Iterations = 500
 
-# This method calls optimize_pulse_unitary
-tlist, coeffs = processor.load_circuit(
-    qc, min_grad=1e-20, init_pulse_type="RND", num_tslots=500,
-    evo_time=2*np.pi, verbose=True
-)
-processor.plot_pulses(
-    title="Control pulse for the Hadamard gate", use_control_latex=False
-)
+Timesteps = 500
 
-rho0 = basis([2, 2], [0, 0])
-plus = (basis(2, 0) + basis(2, 1)).unit()
-minus = (basis(2, 0) - basis(2, 1)).unit()
-result = processor.run_state(init_state=rho0)
-#print("Fidelity:", fidelity(result.states[-1], minus))
+T = 2 * np.pi
 
-# add noise
-processor.t1 = 40.0
-result = processor.run_state(init_state=rho0)
-processor.plot_pulses()
+timespace = np.linspace(0, T, Timesteps)
+
+U_Target = fc.Generate_Rand_Unitary(4)
+H_Drift_Qutip = np.pi * (tensor(sigmaz(), identity(2)) + tensor(identity(2), sigmaz())) + (1/2) * np.pi * tensor(sigmaz(), sigmaz())
+
+H_Drift_Scratch = np.pi * (fc.tensor(fc.sigmaz(), fc.identity(2)) + fc.tensor(fc.identity(2), fc.sigmaz())) + (1/2) * np.pi * fc.tensor(fc.sigmaz(), fc.sigmaz())
+
+H_Control_Qutip = [tensor(sigmax(), identity(2)),
+                   tensor(identity(2), sigmax()),
+                   tensor(sigmax(), sigmax())]
+
+H_Control_Scratch = [fc.tensor(fc.sigmax(), fc.identity(2)),
+                   fc.tensor(fc.identity(2), fc.sigmax()),
+                   fc.tensor(fc.sigmax(), fc.sigmax())]
+
+simulator = Processor(N = N_q)
+
+simulator.add_drift(H_Drift_Qutip, targets = [0, 1])
+
+for operators in H_Control_Qutip:
+    simulator.add_control(operators, targets = [0, 1])
+
+pulses, final_unitary, du_array, cost_fn_array, infidelity_array, energy_array = fc.RunGrapeOptimization(U_Target, H_Drift_Scratch, H_Control_Scratch, Iterations, timespace,
+                                 w_f = 1, w_e = 0, Return_Normalized = False, eps_f = 1, eps_e = 100)
+
+for i in range(len(H_Control_Qutip)):
+    simulator.pulses[i].coeff = pulses[-1, i]
+
+new_timespace = np.append(timespace, timespace[-1])
+simulator.set_all_tlist(new_timespace)
+simulator.plot_pulses()
 plt.show()
-#print("Fidelity with qubit relaxation:", fidelity(result.states[-1], minus))
+
+test = simulator.run_state(init_state = tensor(identity(2), identity(2)), analytical = True)
+
+Unitary_Total = np.eye(4,4)
+
+for x in test:
+    Unitary_Total = x * Unitary_Total
+
+Fidelity = fc.Calculate_Fidelity(U_Target, Unitary_Total)
+
+print("Target Unitary is: ", U_Target)
+print("Final Unitary is: ", Unitary_Total)
+print("Fidelity is: ", Fidelity)
