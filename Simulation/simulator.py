1bf0e5dff72ecac6039660b354f15daef8469d1a
Sebastiaan Fauquenot <102760900+SebFqt@users.noreply.github.com>
2024-01-12 10:46:17 +0100

    Added function explanation and comments

--- Simulation/simulator.py
+++ Simulation/simulator.py
@@ -36,29 +36,30 @@ def CreateEnvironment(N_q, H_Drift, H_Control, T_1, T_2, Timesteps):
     ----------
 
     Environment : Instance of Qutip Processor with all parameters defined in input (qutip.qip.device.processor.Processor object)
-
     """
 
-    T = 2 * np.pi
-    Timespace = np.linspace(0, T, Timesteps)
-    SimulatorTimespace = np.append(Timespace, Timespace[-1])
+    T = 2 * np.pi # Total gate duration
+    
+    Timespace = np.linspace(0, T, Timesteps) # Discretized Time Space
 
-    targets = []
+    SimulatorTimespace = np.append(Timespace, Timespace[-1]) # Customize Time Space for Qutip Processor 
+
+    targets = [] # Create target list based on number of Qubits 
     for i in range(N_q):
         targets.append(i)
     
-    Noise = RelaxationNoise(t1 = T_1, t2 = T_2)
+    Noise = RelaxationNoise(t1 = T_1, t2 = T_2) # Define Noise based on given T1 and T2 time
 
-    Environment = Processor(N = N_q)
+    Environment = Processor(N = N_q) # Initialize Environment using number of Qubits 
 
-    Environment.add_drift(H_Drift, targets = targets)
+    Environment.add_drift(H_Drift, targets = targets) # Specify custom Drift Hamiltonian for the Processor 
 
     for operator in H_Control:
-        Environment.add_control(operator, targets = targets)
+        Environment.add_control(operator, targets = targets) # Specify all operators in H_Control as Control Hamiltonians 
 
-    Environment.set_all_tlist(SimulatorTimespace)
+    Environment.set_all_tlist(SimulatorTimespace) # Set time space of the processor 
 
-    Environment.add_noise(noise = Noise)
+    Environment.add_noise(noise = Noise) # Add Relaxation Noise to processor 
 
     return Environment
 
@@ -81,15 +82,14 @@ def RunPulses(Environment, InitialState, Pulses):
     ----------
 
     result : Result instance of Environment using specified Initial State and Pulse set (np.array (T x N_q^2))
-
     """
 
     for i in range(len(Pulses[:, 0])):
-        Environment.pulses[i].coeff = Pulses[i]
+        Environment.pulses[i].coeff = Pulses[i] # Pass all amplitudes in Pulses array to the Processor 
 
-    result = Environment.run_state(init_state = InitialState)
+    result = Environment.run_state(init_state = InitialState) # Simulate result with specified Environment, Pulses and Initial State
 
-    Environment.plot_pulses()
+    Environment.plot_pulses() # Plot Pulses to check expected result
 
     return result
 
@@ -111,16 +111,18 @@ def CalculateFidelityReward(Result, InitialState, U_Target):
     ----------
 
     r_f : Fidelity Reward for a specific Qutip result, Intital State and Target Unitary (float)
-
     """
 
-    SV_Sim = Result.states[-1]
-    DM_Sim = SV_Sim * SV_Sim.dag()
+    SV_Sim = Result.states[-1] # Define final State Vector (final time step)
 
-    Qutip_U_Target = Qobj(U_Target)
-    SV_Target = Qutip_U_Target * InitialState
-    DM_Target = SV_Target * SV_Target.dag()
+    DM_Sim = SV_Sim * SV_Sim.dag() # Calculate simulated density matrix from state vector (a * a.dag())
 
-    r_f = fidelity(DM_Sim, DM_Target)
+    Qutip_U_Target = Qobj(U_Target) # Convert Target Unitary to a Qobj Object
+
+    SV_Target = Qutip_U_Target * InitialState # Calculate Target State Vector based on Initial State and Target Unitary 
+
+    DM_Target = SV_Target * SV_Target.dag() # Calculate target density matrix from state vector (a * a.dag())
+
+    r_f = fidelity(DM_Sim, DM_Target) # Calculate fidelity between simulated and target density matrix as reward
 
     return r_f
