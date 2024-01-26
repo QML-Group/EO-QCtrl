737b48cac2c9842816456f3b77a8f1be12dd980f
Sebastiaan Fauquenot <102760900+SebFqt@users.noreply.github.com>
2024-01-26 14:43:56 +0100

    Added input file

--- Simulation/main.py
+++ Simulation/main.py
@@ -1,32 +1,10 @@
 import numpy as np 
 from qutip import basis, fidelity, identity, sigmax, sigmaz, tensor, destroy
-from qutip_qip.operations import expand_operator, toffoli, snot
 import functions as fc
 from qutip.metrics import fidelity
 from simulator import QuantumEnvironment
 from QuantumRLagent import QuantumRLAgent
-import tensorflow as tf 
-from scipy.optimize import minimize 
-
-# Input Parameters
-
-h_d = np.pi * (tensor(sigmaz(), identity(2)) + tensor(identity(2), sigmaz())) + (1/2) * np.pi * tensor(sigmaz(), sigmaz()) # Define Drift Hamiltonian used in "Processor"
-h_c = [tensor(identity(2), sigmax())]
-h_l = [r'$u_{1x}$', r'$u_{2x}$', r'$u_{xx}$'] 
-target_unitary = fc.cnot()
-number_qubits = 2
-gate_duration = 2 * np.pi
-t1 = 100 * gate_duration
-t2 = 100 * gate_duration
-number_of_timesteps = 100
-number_of_grape_iterations = 500
-initial_state = basis(4, 2)
-weight_fidelity = 1
-weight_energy = 0
-epsilon_f = 1
-epsilon_e = 100
-n_cycles = 1
-num_episodes = 500
+from input import *
 
 # Test Quantum RL Agent Class
 
