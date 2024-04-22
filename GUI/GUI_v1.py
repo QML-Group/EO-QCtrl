e3d74f8ed50414abd13dcb7e796a1341138412e3
Sebastiaan Fauquenot <102760900+SebFqt@users.noreply.github.com>
2024-04-22 15:45:57 +0200

    More GUI work

--- GUI/GUI_v1.py
+++ GUI/GUI_v1.py
@@ -15,14 +15,14 @@ class EUQOC_App(ctk.CTk):
         self.geometry(f"{1100}x{580}")
 
         # Configure grid layout
-        self.grid_columnconfigure((0, 1, 2, 3), weight = 0)
+        self.grid_columnconfigure((0, 1), weight = 0)
         self.grid_rowconfigure((0, 1, 2, 3, 4, 5, 6, 7), weight = 0)
 
         # Create Input Parameter Section
         self.inputparams_frame = ctk.CTkFrame(self)
         self.inputparams_frame.grid(row = 0, column = 0, padx=(20, 0), pady=(20, 0), sticky = "nsew")
         self.inputparams_label = ctk.CTkLabel(self.inputparams_frame, text = "Input Parameters", font = ctk.CTkFont(size = 20, weight = "bold"))
-        self.inputparams_label.grid(row = 0, column = 0, padx = 10, pady = (20, 10))
+        self.inputparams_label.grid(row = 0, column = 1, padx = 10, pady = (20, 10))
 
         self.u_t_optionmenu = ctk.CTkOptionMenu(self.inputparams_frame, dynamic_resizing=False,
                                                 values=["CNOT", "Hadamard", "T"], fg_color="white", text_color="green")
@@ -39,13 +39,13 @@ class EUQOC_App(ctk.CTk):
         self.h_c_optionmenu.grid(row = 2, column = 0, padx = 20, pady = (20, 10))
         self.h_c_optionmenu.set("Control Hamiltonian")
 
-        self.t_1_optionmenu = ctk.CTkEntry(self.inputparams_frame, placeholder_text = "Decoherence Time", fg_color="white", text_color="green")
+        self.t_1_optionmenu = ctk.CTkEntry(self.inputparams_frame, placeholder_text = "Decoherence Time", fg_color="white", text_color="green", placeholder_text_color= "green")
         self.t_1_optionmenu.grid(row = 2, column = 1, padx = 20, pady = (20, 10))
 
-        self.N_g_optionmenu = ctk.CTkEntry(self.inputparams_frame, placeholder_text = "GRAPE Iterations", fg_color="white", text_color="green")
+        self.N_g_optionmenu = ctk.CTkEntry(self.inputparams_frame, placeholder_text = "GRAPE Iterations", fg_color="white", text_color="green", placeholder_text_color= "green")
         self.N_g_optionmenu.grid(row = 1, column = 2, padx = 20, pady = (20, 10))
 
-        self.N_t_optionmenu = ctk.CTkEntry(self.inputparams_frame, placeholder_text = "Training Episodes", fg_color="white", text_color="green")
+        self.N_t_optionmenu = ctk.CTkEntry(self.inputparams_frame, placeholder_text = "Training Episodes", fg_color="white", text_color="green", placeholder_text_color= "green")
         self.N_t_optionmenu.grid(row = 2, column = 2, padx = 20, pady = (20, 10))
 
 
@@ -54,7 +54,7 @@ class EUQOC_App(ctk.CTk):
         self.algoselect_frame = ctk.CTkFrame(self)
         self.algoselect_frame.grid(row = 1, column = 0, padx = (20, 0), pady = (20, 0), sticky = "nsew")
         self.algoselect_label = ctk.CTkLabel(self.algoselect_frame, text = "Algorithm Selector", font = ctk.CTkFont(size = 20, weight = "bold"))
-        self.algoselect_label.grid(row = 0, column = 0, padx = 10, pady = (20, 10))
+        self.algoselect_label.grid(row = 0, column = 1, padx = 10, pady = (20, 10))
 
         self.eo_grape_switch = ctk.CTkSwitch(master = self.algoselect_frame, text = "EO-GRAPE")
         self.eo_grape_switch.grid(row = 1, column = 0, padx = 20, pady = (20, 10))
@@ -70,7 +70,7 @@ class EUQOC_App(ctk.CTk):
         self.weightselect_frame = ctk.CTkFrame(self)
         self.weightselect_frame.grid(row = 2, column = 0, padx = (20, 0), pady = (20, 0), sticky = "nsew")
         self.weightselect_label = ctk.CTkLabel(self.weightselect_frame, text = "Weight Selector", font = ctk.CTkFont(size = 20, weight = "bold"))
-        self.weightselect_label.grid(row = 0, column = 0, padx = 10, pady = (20, 10))
+        self.weightselect_label.grid(row = 0, column = 1, padx = 10, pady = (20, 10))
         self.w_e_label = ctk.CTkLabel(self.weightselect_frame, text = "Weight EC", font = ctk.CTkFont(size = 14))
         self.w_e_label.grid(row = 1, column = 0, padx = 10, pady = (20, 10))
         self.weightselect_slider_1 = ctk.CTkSlider(self.weightselect_frame, orientation = "horizontal", from_=0, to=1, number_of_steps=10, width = 300, progress_color="green", button_hover_color="darkgreen")
@@ -80,6 +80,19 @@ class EUQOC_App(ctk.CTk):
         self.weightselect_slider_2 = ctk.CTkSlider(self.weightselect_frame, orientation = "horizontal", from_=0, to=1, number_of_steps=10, width = 300, progress_color="red", button_hover_color="darkred", button_color="red")
         self.weightselect_slider_2.grid(row = 2, column = 1)
 
+        # Run Button
+
+        self.runbutton_frame = ctk.CTkFrame(self)
+        self.runbutton_frame.grid(row = 3, column = 0, padx = (20, 0), pady = (20, 0), sticky = "nsew")
+        self.runbutton = ctk.CTkButton(self.runbutton_frame, text="Run Experiment", width = 600)
+        self.runbutton.grid(column = 1)
+
+        # Results frame
+
+        self.results_frame = ctk.CTkFrame(self)
+        self.results_frame.grid(row = 0, column = 1, padx = (20, 0), pady = (20, 0), sticky = "nsew", rowspan = 4)
+        self.results_label = ctk.CTkLabel(self.results_frame, text = "Simulation Results", font = ctk.CTkFont(size = 20, weight = "bold"))
+        self.results_label.grid(row = 0, column = 0, padx = 10, pady = (20, 10))
 
 if __name__ == "__main__":
     app = EUQOC_App()
