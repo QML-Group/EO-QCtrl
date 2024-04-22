f527a6733906633548197f7adf9b9a167bd03fe4
Sebastiaan Fauquenot <102760900+SebFqt@users.noreply.github.com>
2024-04-22 11:52:02 +0200

    GUI

--- GUI/testgui.py
+++ GUI/testgui.py
@@ -1,7 +1,10 @@
 import tkinter as tk
 
+def function():
+    print("Button Test")
+
 window = tk.Tk()
-btn = tk.Button(window, text = "Button test widget", fg = "blue")
+btn = tk.Button(window, text = "Button test widget", fg = "blue", command = function)
 btn.place(x=80, y=100)
 window.title("Test GUI")
 window.geometry("300x200+10+10")
